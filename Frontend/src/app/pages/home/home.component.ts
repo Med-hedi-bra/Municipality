47b0c14201f9bcecd2799e95bee82c70ad2b96fc
Wassef Chebbi <wassef@MacBook-Air-de-Wassef.local>
2023-05-08 16:09:32 +0100

    lst updates before presentation

--- Frontend/src/app/pages/home/home.component.ts
+++ Frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticateService } from 'src/app/services/auth/authenticate.service';
+import { RegisterRequestPayload } from '../register/register-request.payload';
+import { User } from '../../model/user.model';
+
 
 
 
@@ -7,7 +12,42 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent  {
-  constructor(){}
+export class HomeComponent implements OnInit {
+  registerRequestPayload!: RegisterRequestPayload;
+  user!: User;
+
+
+  constructor(
+    private authService: AuthenticateService,
+    private http: HttpClient
+  ){}
+  
+  ngOnInit() {
+
+    if(this.authService.isAuthenticated()){
+
+    this.user =this.authService.currentUser;
+    console.log(this.user);
+    console.log(this.authService.getMun());
+    }
+
+    
+   
+   
+   
+    
+    // cin: this.authService.currentUser.get('cin').value;
+    // codeMun: this.authService.currentUser.get('codeMun').value;
+    // console.log(cin);
+   
+   
+   
+   
+
+   this.http.get("http://localhost:8080/municipality/user/");
+   this.http.get("http://localhost:8080/municipality/user/");
+  }
+
+  
  
 }
